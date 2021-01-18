e1da318d9047c9a51ed8561e74b0c0a70ea2547b
cookie777 <l.oeil.ballon@gmail.com>
2021-01-18 10:52:30 -0800

    refactor(mainVC):

--- Hang-Out Planner/ViewController/MainViewController.swift
+++ Hang-Out Planner/ViewController/MainViewController.swift
@@ -24,6 +24,8 @@ class MainViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
+    
+    //Add goButton to view (you can modify this)
     view.addSubview(goButton)
     goButton.centerXYinSafeArea(view)
     goButton.addTarget(self, action: #selector(goButtonTapped), for: .touchUpInside)
@@ -31,9 +33,14 @@ class MainViewController: UIViewController {
     
   }
   
+  //Action when goButton is tapped
   @objc func goButtonTapped(){
+    
+    //Send selectedCategories to logic model
     let plans = Planner.calculatePlans(categories: selectedCategories)
     let nextVC = PlanListTableViewController(plans: plans)
+    
+    // Move to next VC
     navigationController?.pushViewController(nextVC, animated: true)
   }
 }

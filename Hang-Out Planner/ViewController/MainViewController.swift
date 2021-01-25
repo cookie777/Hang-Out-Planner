0519554f08dd901f69313f75e0866ba67ee8a1bc
kengo-taka <1117marine1117@gmail.com>
2021-01-25 15:38:34 -0800

    kengo-locationManager2

--- Hang-Out Planner/ViewController/MainViewController.swift
+++ Hang-Out Planner/ViewController/MainViewController.swift
@@ -325,33 +325,6 @@ extension MainViewController: CLLocationManagerDelegate{
         mapView.addAnnotation(annotation)
     self.mapView.setRegion(region, animated: true)
    
-    
-    func lookUpCurrentLocation(completionHandler: @escaping (CLPlacemark?)
-                    -> Void ) {
-        // Use the last reported location.
-      if let lastLocation = self.locationManager?.location {
-            let geocoder = CLGeocoder()
-                
-            // Look up the location and pass it to the completion handler
-            geocoder.reverseGeocodeLocation(lastLocation,
-                                            completionHandler: { [self] (placemarks, error) in
-                if error == nil {
-                    let firstLocation = placemarks?[0]
-                    completionHandler(firstLocation)
-                  locationLabel.text = firstLocation?.name
-                  print(firstLocation?.name as Any)
-                }
-                else {
-               // An error occurred during geocoding.
-                    completionHandler(nil)
-                }
-            })
-        }
-        else {
-            // No location was available.
-            completionHandler(nil)
-        }
-    }
     // update pin(annotation) location
     // at later here !
   }
